66f2c1cd7883163e65a7de66d83663a648ab4517
aprnabagchi <71764327+aprnabagchi@users.noreply.github.com>
2021-03-31 14:17:29 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,12 @@
+import firebase from  'firebase'
+require ('@firebase/firestore')
+var firebaseConfig = {
+  apiKey: "AIzaSyDGNHMcG6OtOuCNBR7tHs_lx3DXBlFN8IE",
+  authDomain: "booksantaapp-dfe9d.firebaseapp.com",
+  projectId: "booksantaapp-dfe9d",
+  storageBucket: "booksantaapp-dfe9d.appspot.com",
+  messagingSenderId: "160399703411",
+  appId: "1:160399703411:web:bc61c2e9ad7320d6ded906"
+};
+firebase.initializeApp(firebaseConfig)
+export default firebase.firestore()
